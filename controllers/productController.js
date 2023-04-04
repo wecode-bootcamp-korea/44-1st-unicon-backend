92192df20c523300b168dd7c62972b004552cf9e
김민서 <gimminseo@minseo-mac.local>
2023-04-04 18:28:39 +0900

    checkout

---
+++ controllers/productController.js
@@ -0,0 +1,8 @@
+const postService = require('../services/productService');
+const { catchErr } = require('../middleware/error');
+
+const getProductByProductId = catchErr(async (req, res) => {
+  const { productId, smallCategory } = req.body;
+  const product = await productService.getProductByProductId(productId);
+  return res.status(200).json({ product });
+});
--- routes/productRouter.js
+++ routes/productRouter.js
@@ -1,6 +1,10 @@
 const express = requrie('express');
-const productController = require('../controller/productController');
+const productController = require('../controllers/productController');
 const router = express.Router();
 //const validateToken = require('../middleware/auth')
 
 router.get('/:productId', productController.getProductByProductId);
+
+module.exports = {
+  router,
+};
