a4ab27a3a965f96925f9e15fdefc1177f3a14cc5
SH <robot4097@gmail.com>
2023-04-08 13:47:45 +0900

    whip

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -15,28 +15,6 @@ const createOrders = catchError(async (req, res) => {
 });
 
 
-// const createOrderStatus = catchError(async (req, res) => {
-//   const { status, orderId } = req.body;
-//   if (!status || !orderId) {
-//     return res.status(400).json({ message: 'KEY_ERROR' });
-//   }
-//   await orderService.createOrderStatus(status, orderId);
-//   return res.status(201).json({ message: 'CHECKED_ORDER_STATUS' });
-// });
-
-
-// const createOrderItemStatus = catchError(async (req, res) => {
-//   const { status, orderItemId } = req.body;
-//   if (!status || !orderItemId) {
-//     return res.status(400).json({ message: 'KEY_ERROR' });
-//   }
-//   await orderSerivce.createOrderItemStatus(status, orderItemId);
-//   return res.status(201).json({ message: 'CHECKED_ITEM_STATUS' });
-// });
-
-
 module.exports = {
   createOrders,
-//   createOrderStatus,
-//   createOrderItemStatus,
 };
