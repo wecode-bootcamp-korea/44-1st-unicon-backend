bfed3ee880305461e6757f830470757d3f8b5eb0
SH <robot4097@gmail.com>
2023-04-10 23:07:05 +0900

    fix_erd

--- db/migrations/20230410132505_whip2.sql
+++
@@ -1,8 +0,0 @@
--- migrate:up
-
-UPDATE order_status SET status = 'PENDING_PAYMENT' WHERE id = 1;
-UPDATE order_status SET status = 'COMPLETED_PAYMENT' WHERE id = 2;
-UPDATE order_status SET status = 'DELIVERING' WHERE id = 3;
-UPDATE order_status SET status = 'DELIVERY_COMPLETED' WHERE id = 4;
--- migrate:down
-
