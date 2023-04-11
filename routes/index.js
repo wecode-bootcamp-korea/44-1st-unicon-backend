e89e5861ddec4c76aa3f5bb0260466e9bc4bc7bd
Lim seong-geun <123074891+lsg622@users.noreply.github.com>
2023-04-11 16:39:48 +0900

    [Modify]: routes/index.js 수정

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const { loginRequired } = require('../middlewares/auth');
-const productRouter = require('./productRouter');
 
+const productRouter = require('./productRouter');
 const userRouter = require('./userRouter');
+const cartRouter = require('./cartRouter');
+const paymentRouter = requrie('./paymentRouter');
 
 router.use('/users', userRouter.router);
 router.use('/products', productRouter.router);
+router.use('/cart', cartRouter);
+router.use('/payment', paymentRouter.router);
 
 module.exports = router;
