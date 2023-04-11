4fcb059472eb79f58c3293feeb443f8859a6fc43
Lim seong-geun <123074891+lsg622@users.noreply.github.com>
2023-04-11 15:48:30 +0900

    wip

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,10 @@ const router = express.Router();
 const cartRouter = require('./cartRouter');
 const userRouter = require('./userRouter');
 const productRouter = require('./productRouter');
-
 const paymentRouter = require('./paymentRouter');
 
 router.use('/users', userRouter.router);
 router.use('/products', productRouter.router);
-
 router.use('/payment', paymentRouter.router);
 router.use('/cart', cartRouter);
 
--- routes/paymentRouter.js
+++ routes/paymentRouter.js
@@ -3,6 +3,8 @@ const paymentController = require('../controllers/paymentController');
 const { loginRequired } = require('../middlewares/auth');
 const router = express.Router();
 
+const transaction = await sequelize.transaction();
+
 router.post('', loginRequired, paymentController.createPayment);
 module.exports = {
   router,
